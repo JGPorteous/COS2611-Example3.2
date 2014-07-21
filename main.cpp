0e206c743bb649c9c94e5035da577c476757b238
Justin Porteous <justin@porteous.co.za>
2014-07-21 17:19:58 +0200

    Code completed and tested

--- main.cpp
+++ main.cpp
@@ -4,6 +4,32 @@ using namespace std;
 
 int main()
 {
+    int *p;
+    int num1 = 5;
+    int num2 = 8;
+
+    p = &num1; // p = address of num1
+
+    cout << "First cycle - &num1 = " << &num1
+        << ", p = " << p << endl;
+    cout << "    num1 = " << num1
+        << ", *p = " << *p << endl;
+
+    p = &num2; //p = address of num2
+
+    cout << "Second cycle - &num2 = " << &num2
+        << ", p = " << p << endl;
+    cout << "    num1 = " << num1
+        << ", *p = " << *p << endl;  //*p = the value of the variable which p points to
+
+    *p = 2 * (*p);  // this means the variable p points to equals 2 times the variable p points to
+                    // equiv: num2 = 2 * num2;
+
+    cout << " Final cycle - num2 = " << num2
+        << ", *p = " << *p << endl;
+
+
+
     cout << "Hello world!" << endl;
     return 0;
 }
